01c44acb182716e038bc6181e2b253355c89c493
Edson Bruno <edsonbruno415@gmail.com>
2025-08-27 00:02:56 -0300

    fix: status code route

--- src/main/java/com/edson/proposta_app/controller/PropostaController.java
+++ src/main/java/com/edson/proposta_app/controller/PropostaController.java
@@ -4,12 +4,12 @@ import com.edson.proposta_app.dto.PropostaRequestDto;
 import com.edson.proposta_app.dto.PropostaResponseDto;
 import com.edson.proposta_app.service.PropostaService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @AllArgsConstructor
 @RestController
@@ -21,6 +21,11 @@ public class PropostaController {
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto){
         PropostaResponseDto response = propostaService.criar(requestDto);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(response.getId())
+                        .toUri())
+                        .body(response);
     }
+
 }
